1e192d5dbe57455a1bf77eaa461791e73f678ab8
Ray Ma <marui0709usa@gmail.com>
2026-06-30 04:34:21 +0000

    fix trailer number data type

--- models/staging/stg_insurance_claims.sql
+++ models/staging/stg_insurance_claims.sql
@@ -22,7 +22,7 @@ renamed as (
     select
         -- Primary key
         trim(claim_number)                              as claim_number,
-        cast(submitted as date)                         as submitted_date,
+        
         
         -- Dimensions
         trim(carrier_name)                              as carrier_name,
@@ -33,16 +33,17 @@ renamed as (
         trim(insurance_claims_results)                  as insurance_claims_results,
         trim(source)                                    as source,
         trim(trailer_license_plate)                     as trailer_license_plate,
-        trim(trailer_number)                            as trailer_number,
+        trim(cast(trailer_number as string))            as trailer_number,
         trim(trailer_vin)                               as trailer_vin,
-        upper(trim(currency))                          as currency,
+        upper(trim(currency))                           as currency,
 
         -- Dates
-        
+        cast(submitted as date)                         as submitted_date,
         cast(date_of_loss as date)                      as date_of_loss,
         cast(dropped_off_date as date)                  as dropped_off_date,
         cast(case_date_closed as date)                  as case_date_closed,
         cast(last_seen_date as date)                    as last_seen_date,
+        
 
         -- Numeric
         cast(value_of_trailer as decimal(12,2))         as value_of_trailer,
